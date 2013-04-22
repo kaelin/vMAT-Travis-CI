96ecbe6c867ff147a9422a00b4c3abf8cb7bbb74
Kaelin Colclasure <kaelin@acm.org>
2013-04-21 18:10:54 -0700

    Initial Commit

---
+++ vMAT-Travis-CI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  vMAT-Travis-CI
+//
+//  Created by Kaelin Colclasure on 4/21/13.
+//  Copyright (c) 2013 Kaelin Colclasure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
