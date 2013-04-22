0f83f6e70feb6eb9912f4c463002579895c07e36
Kaelin Colclasure <kaelin@acm.org>
2013-04-22 13:46:50 -0700

    Add some code to main for test purposes.

--- vMAT-Travis-CI/main.mm
+++ vMAT-Travis-CI/main.mm
@@ -8,14 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+#import <vMAT.h>
+
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
+        vMAT_Array * matA = vMAT_eye(vMAT_MakeSize(13), nil);
+        NSLog(@"%@", matA.dump);
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int width = 70;
+        char bar[width - 1];
+        memset(bar, ' ', width - 2);
+        bar[width - 2] = '\0';
         
+        for (float pct = 0;
+             pct <= 1.01;
+             pct += 0.1) {
+            memset(bar, '#', pct * (width - 2));
+            printf("  [%s] %3.0f%%\r", bar, pct * 100);
+            fflush(stdout);
+            [NSThread sleepForTimeInterval:0.1];
+        }
+        printf("\n");
     }
     return 0;
 }
